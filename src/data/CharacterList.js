a6cb6d7ac3e32275da7aca4f1b5e5e78e258dc19
MaheriniainANdria <mahery2929@gmail.com>
2024-07-24 15:26:04 +0300

    feat: change the structure with counter features

--- src/data/CharacterList.js
+++ src/data/CharacterList.js
@@ -5,4 +5,30 @@ export const listCharacters = [
     { id: 2, name: 'Paul', instrument: 'Bass' },
     { id: 3, name: 'George', instrument: 'Guitar' },
     { id: 4, name: 'Ringo', instrument: 'Drums' },
+    { id: 5, name: 'Mick', instrument: 'Vocals' },         // Mick Jagger
+    { id: 6, name: 'Keith', instrument: 'Guitar' },        // Keith Richards
+    { id: 7, name: 'Ronnie', instrument: 'Guitar' },       // Ronnie Wood
+    { id: 8, name: 'Charlie', instrument: 'Drums' },       // Charlie Watts
+    { id: 9, name: 'Freddie', instrument: 'Vocals' },      // Freddie Mercury
+    { id: 10, name: 'Brian', instrument: 'Guitar' },       // Brian May
+    { id: 11, name: 'Roger', instrument: 'Drums' },        // Roger Taylor
+    { id: 12, name: 'John', instrument: 'Bass' },          // John Deacon
+    { id: 13, name: 'Robert', instrument: 'Vocals' },      // Robert Plant
+    { id: 14, name: 'Jimmy', instrument: 'Guitar' },       // Jimmy Page
+    { id: 15, name: 'John', instrument: 'Drums' },         // John Bonham
+    { id: 16, name: 'John', instrument: 'Bass' },          // John Paul Jones
+    { id: 17, name: 'Kurt', instrument: 'Guitar' },        // Kurt Cobain
+    { id: 18, name: 'Krist', instrument: 'Bass' },         // Krist Novoselic
+    { id: 19, name: 'Dave', instrument: 'Drums' },         // Dave Grohl
+    { id: 20, name: 'David', instrument: 'Vocals' },       // David Bowie
+    { id: 21, name: 'Elton', instrument: 'Piano' },        // Elton John
+    { id: 22, name: 'Slash', instrument: 'Guitar' },       // Slash
+    { id: 23, name: 'Axl', instrument: 'Vocals' },         // Axl Rose
+    { id: 24, name: 'Duff', instrument: 'Bass' },          // Duff McKagan
+    { id: 25, name: 'Steven', instrument: 'Drums' },       // Steven Adler
+    { id: 26, name: 'Jimi', instrument: 'Guitar' },        // Jimi Hendrix
+    { id: 27, name: 'Noel', instrument: 'Guitar' },        // Noel Gallagher
+    { id: 28, name: 'Liam', instrument: 'Vocals' },        // Liam Gallagher
+    { id: 29, name: 'Eddie', instrument: 'Guitar' },       // Eddie Van Halen
+    { id: 30, name: 'Alex', instrument: 'Drums' },         // Alex Van Halen
   ];
--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,96 +1,81 @@
 /* eslint-disable prettier/prettier */
 // src/views/HomeView/HomeView.tsx
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ImageBackground, FlatList, Button, Alert } from 'react-native';
-//import { Character } from '../../models/Character';
+import { View, Text, StyleSheet, ImageBackground, Button } from 'react-native';
 import { listCharacters } from '../../data/CharacterList';
 
 const HomeView = (): React.JSX.Element => {
+  const [counterStar, setCounterStar] = useState(0);
 
-    const [counterStar, setCounterStar] = useState(0);
-
-    const onNext = () => {
-        setCounterStar(counterStar + 1);
-    }
-
-    const onPrevious = () => {
-        setCounterStar(counterStar - 1);
-    }
-
-    const onReset = () => {
-        setCounterStar(0)
-    }
-
-    return (
-      <ImageBackground 
-        source={require('../../assets/images/bg.jpg')} 
-        style={styles.backgroundImage}
-      >
-        <View style={styles.container}>
-        <Button
-        title="Next"
-        onPress={() => onNext()}
-      />
-          <Text style={styles.text}>You clicked the NEXT button {counterStar}times </Text>
-          <Button
-        title="Minus 1"
-        onPress={() => onPrevious()}
-      />
-      <Button
-        title="Reset"
-        onPress={() => onReset()}
-      />
-          <FlatList
-            data={listCharacters} //.reverse(), make the list reverse
-            keyExtractor={item => item.id.toString()}
-            renderItem={({ item }) => (
-              <CharacterInfo id={item.id} name={item.name} instrument={item.instrument} />
-            )}
-          />
-        </View>
-      </ImageBackground>
-    );
+  const onNext = () => {
+    setCounterStar((prevCounter) => (prevCounter + 1) % listCharacters.length);
   };
 
-  type CharacterProps = {
-    id: number;
-    name: string;
-    instrument: string;
+  const onPrevious = () => {
+    setCounterStar((prevCounter) => (prevCounter - 1 + listCharacters.length) % listCharacters.length);
   };
 
-  const CharacterInfo = ({ name, instrument }: CharacterProps) => {
-    return (
-      <View style={styles.characterContainer}>
-        <Text style={styles.text}>My name is {name}</Text>
-        <Text style={styles.text}>I play {instrument}</Text>
+  const onReset = () => {
+    setCounterStar(0);
+  };
+
+  const currentCharacter = listCharacters[counterStar];
+
+  return (
+    <ImageBackground 
+      source={require('../../assets/images/bg.jpg')} 
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>You are viewing {currentCharacter.name}</Text>
+        <CharacterInfo id={currentCharacter.id} name={currentCharacter.name} instrument={currentCharacter.instrument} />
+        <Button title="Next" onPress={onNext} />
+        <Button title="Previous" onPress={onPrevious} />
+        <Button title="Reset" onPress={onReset} />
       </View>
-    );
-  };
+    </ImageBackground>
+  );
+};
 
-  const styles = StyleSheet.create({
-    backgroundImage: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-      padding: 20,
-    },
-    characterContainer: {
-      marginBottom: 20,
-      padding: 10,
-      backgroundColor: '#f0f0f0',
-      borderRadius: 5,
-    },
-    text: {
-      color: '#000',
-      fontSize: 18,
-      margin: 10,
-    },
-  });
+type CharacterProps = {
+  id: number;
+  name: string;
+  instrument: string;
+};
 
-  export default HomeView;
+const CharacterInfo = ({ name, instrument }: CharacterProps) => {
+  return (
+    <View style={styles.characterContainer}>
+      <Text style={styles.text}>My name is {name}</Text>
+      <Text style={styles.text}>I play {instrument}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: 20,
+  },
+  characterContainer: {
+    marginBottom: 20,
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
+  },
+  text: {
+    color: '#000',
+    fontSize: 18,
+    margin: 10,
+  },
+});
+
+export default HomeView;
